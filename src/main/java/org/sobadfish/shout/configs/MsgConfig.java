1d403f261157cc80daa083336323543da4117114
Sobadfish <3098143605>
2022-09-07 16:20:13 +0800

    修复乱码问题

--- src/main/java/org/sobadfish/shout/configs/MsgConfig.java
+++ src/main/java/org/sobadfish/shout/configs/MsgConfig.java
@@ -2,9 +2,14 @@ package org.sobadfish.shout.configs;
 
 import java.util.List;
 
+
 public class MsgConfig {
 
+    public String text;
 
+    public int type;
 
+    public boolean check;
 
+    public List<String> word;
 }
